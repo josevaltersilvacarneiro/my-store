fe462659198df2145e363fafee1d56d23f115982
José Valter Silva Carneiro <git@josevaltersilvacarneiro.net>
2022-11-23 19:16:59 -0300

    The layout was created

---
+++ App/View/Layout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+	<head>
+		<meta charset='UTF-8'>
+		
+		<title><?=$this->get_title()?></title>
+	</head>
+	<body>
+		<header>
+			<?=$this->add_header()?>
+		</header>
+		<main>
+			<?=$this->add_main()?>
+		</main>
+		<footer>
+			<?=$this->add_footer()?>
+		</footer>
+	</body>
+</html>
